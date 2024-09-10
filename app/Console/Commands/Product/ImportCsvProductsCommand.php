04b13faf60d19d9b1b1c2d96f6a7bf6da1b7a7fa
Leon Czerwinski <leon.czerwinski@pergam.in>
2024-09-10 16:09:55 +0200

    Designed basic app structure - added empty command, model service and repo related to product

---
+++ app/Console/Commands/Product/ImportCsvProductsCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands\Product;
+
+use Illuminate\Console\Command;
+
+class ImportCsvProductsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'products:import';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+    }
+}
---
+++ app/Repositories/Product/Dto/ProductDto.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\Product\Dto;
+
+readonly class ProductDto
+{
+    public function __construct()
+    {
+    }
+}
